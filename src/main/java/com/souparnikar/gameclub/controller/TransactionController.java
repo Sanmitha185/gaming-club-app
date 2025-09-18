98cf60fd5ec6c8c71123faf8fab1bf347128c89e
Sanmitha185 <sanmithasrivatsa2@gmail.com>
2025-09-18 15:15:06 +0530

    Update TransactionController.java

--- src/main/java/com/souparnikar/gameclub/controller/TransactionController.java
+++ src/main/java/com/souparnikar/gameclub/controller/TransactionController.java
@@ -1,12 +1,16 @@
 package com.souparnikar.gameclub.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.souparnikar.gameclub.modal.Transaction;
 import com.souparnikar.gameclub.repository.TransactionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/transactions")
@@ -15,12 +19,12 @@ public class TransactionController {
     @Autowired
     private TransactionRepository repo;
 
-    @PostMapping
-    public Transaction create(@RequestBody Transaction transaction) {
-        transaction.setId(null); // Let MongoDB generate ObjectId
-        transaction.setDateTime(new Date()); // Default to now
-        return repo.save(transaction);
-    }
+    // @PostMapping
+    // public Transaction create(@RequestBody Transaction transaction) {
+    //     transaction.setId(null); // Let MongoDB generate ObjectId
+    //     transaction.setDateTime(new Date()); // Default to now
+    //     return repo.save(transaction);
+    // }
 
     @GetMapping
     public List<Transaction> findAll() {
@@ -32,16 +36,16 @@ public class TransactionController {
         return repo.findById(id).orElse(null);
     }
 
-    @PutMapping("/{id}")
-    public Transaction update(@PathVariable String id, @RequestBody Transaction transaction) {
-        return repo.findById(id).map(old -> {
-            old.setMemberId(transaction.getMemberId());
-            old.setGameId(transaction.getGameId());
-            old.setAmount(transaction.getAmount());
-            old.setDateTime(transaction.getDateTime() != null ? transaction.getDateTime() : old.getDateTime());
-            return repo.save(old);
-        }).orElse(null);
-    }
+    // @PutMapping("/{id}")
+    // public Transaction update(@PathVariable String id, @RequestBody Transaction transaction) {
+    //     return repo.findById(id).map(old -> {
+    //         old.setMemberId(transaction.getMemberId());
+    //         old.setGameId(transaction.getGameId());
+    //         old.setAmount(transaction.getAmount());
+    //         old.setDateTime(transaction.getDateTime() != null ? transaction.getDateTime() : old.getDateTime());
+    //         return repo.save(old);
+    //     }).orElse(null);
+    // }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable String id) {
